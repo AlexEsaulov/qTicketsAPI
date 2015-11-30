303cedca008d90fea41816c4e068ee0544cecc2c
Alexandr Esaulov <alex.esaulov@inbox.ru>
2015-11-30 21:19:42 +0300

    Payment proxy

--- routes.php
+++ routes.php
@@ -1,14 +1,31 @@
 <?php
-use Idesigning\QTicketsAPI\Facades\Order;
-/*
-Route::get('test_api', function () {
 
-    var_dump(Order::findOrFail(25)->event->currency->name);
+use Idesigning\QTicketsAPI\Models\Settings;
 
+Route::any('qtickets', function () {
 
-    Order::where('id', '>', '20')->orderBy('id', 'asc')->get()->each(function ($order) {
+    $url = Settings::get('url', 'http://qtickets.ru/api');
 
-        var_dump($order->toArray());
+    $headers[] = 'Content-Type: ' . Request::server('HTTP_CONTENT_TYPE', 'application/x-www-form-urlencoded');
 
-    });
-});*/
+    if (Request::server('HTTP_CONTENT_LENGTH')) {
+        $headers[] = 'Content-Length: ' . Request::server('HTTP_CONTENT_LENGTH');
+    }
+
+    $response = file_get_contents(rtrim($url, '/') . '/pay' . '?' . http_build_query(get()), null, stream_context_create(array(
+        'http' => array(
+            'method' => 'POST',
+            'header' => join(PHP_EOL, $headers),
+            'content' => file_get_contents('php://input'),
+            'ignore_errors' => true
+        ),
+    )));
+
+    $data = json_decode($response);
+
+    if (isset($data->status) && $data->status == 200) {
+        return response($data->content, $data->status, $data->headers);
+    }
+
+    return response($response, 500);
+});
